a9f59c8ff7d164bd4be8358086c048560a712c67
Joseph Kim <jkim0120@hotmail.com>
2015-01-06 23:48:05 -0500

    Add welcome title

---
+++ js/unsemantic.js
@@ -0,0 +1,9 @@
+var ADAPT_CONFIG = {
+  path: 'css/',
+  dynamic: true,
+  range: [
+    '0px    to 767px  = unsemantic-grid-mobile.css',
+    '767px  to 1024px  = unsemantic-grid-tablet.css',
+    '1024px = unsemantic-grid-desktop.css'
+  ]
+};
