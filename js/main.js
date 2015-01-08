fd074794de7af81e25d1fedcee570f5093275147
Joseph Kim <jkim0120@hotmail.com>
2015-01-08 05:19:31 -0500

    Add fade animation and fix viewport optimization

--- js/main.js
+++ js/main.js
@@ -4,12 +4,25 @@ function changeMobile() {
 	if (window.innerWidth < 768) {
 		document.getElementById('jump').className = "grid_12";
 		document.getElementById('greeting').className = "grid_12";
+		document.getElementById('greeting2').className = "fadeIn grid_12";
 	}
 	else {
 		document.getElementById('jump').className = "grid_6";
 		document.getElementById('greeting').className = "grid_6";
+		document.getElementById('greeting2').className = "fadeIn grid_6";
 	}
 }
+changeMobile();
+window.onresize = changeMobile;
+window.onresize = changeMobile;
 
-window.onresize = changeMobile;
-window.onresize = changeMobile;
+// Load fade-in Welcome
+$(document).ready(function() {
+    $('.fadeIn').hide();
+});
+
+$(window).load(function() {
+    $('.fadeIn').each(function(i) {
+       $(this).delay((i + 1) * 1000).fadeIn(1500);
+    });
+});
