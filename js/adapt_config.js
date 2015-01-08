f4e5928f64b815b22e6657fc47d1999b7681d9d2
Joseph Kim <jkim0120@hotmail.com>
2015-01-07 22:36:04 -0500

    Replace Unsemantic with 960.gs

---
+++ js/adapt_config.js
@@ -0,0 +1,9 @@
+var ADAPT_CONFIG = {
+  path: 'css/',
+  dynamic: true,
+  range: [
+    '0px    to 760px  = mobile.min.css',
+    '760px  to 1240px  = tablet.min.css',
+    '1240px = desktop.min.css'
+  ]
+};
