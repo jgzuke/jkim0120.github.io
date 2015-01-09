60b0b813b53ac1c059167ec61e7d0dfdeb1109d2
Joseph Kim <jkim0120@hotmail.com>
2015-01-09 01:27:12 -0500

    Fix mobile view

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ function changeMobile() {
 	if (window.innerWidth < 768) {
 		document.getElementById('jump').className = "grid_12";
 		document.getElementById('greeting').className = "grid_12";
-		document.getElementById('greeting2').className = "fadeIn grid_12";
+		document.getElementById('greeting2').className = "fadeUp grid_12";
 	}
 	else {
 		document.getElementById('jump').className = "grid_6";
 		document.getElementById('greeting').className = "grid_6";
-		document.getElementById('greeting2').className = "fadeIn grid_6";
+		document.getElementById('greeting2').className = "fadeUp grid_6";
 	}
 }
 changeMobile();
@@ -18,12 +18,13 @@ window.onresize = changeMobile;
 
 // Load fade-in Welcome
 $(document).ready(function() {
-    $('.fadeIn').css({marginTop: 50, opacity: 0});
+    $('.fadeIn').css({opacity: 0});
+    $('.fadeUp').css({marginTop: 50, opacity: 0});
 });
 
 $(window).load(function() {
-    $('.fadeIn').each(function(i) {
-       $(this).delay((i + 1) * 750).animate({marginTop: 0, opacity: 1}, 1250);
+    $('.fadeIn').delay(700).animate({opacity: 1}, 1200, function(){
+       $('.fadeUp').animate({marginTop: 0, opacity: 1}, 900);
     });
 });
 
