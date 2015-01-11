9f0221be18551b0105906b14692290b085c1530e
Joseph Kim <jkim0120@hotmail.com>
2015-01-10 22:52:22 -0500

    Complete text display navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // On page load-up
-var navbar = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9];
+var navbar = [c0, c1, c2, c3, c4, c5, c6];
+var navlinks = [l0, l1, l2, l3, l4, l5, l6];
+var navpage = [p0, p1, p2, p3, p4, p5, p6];
 
 $(document).ready(function() {
     // Welcome init
@@ -7,18 +9,25 @@ $(document).ready(function() {
     $('.fadeUp').css({marginTop: 50, opacity: 0});
 
     // Navbar init
-    for(var i = 0; i < 10; i++) {
+    for(var i = 0; i < 7; i++) {
     	navbar[i] = document.getElementById("c" + i.toString());
+    	navlinks[i] = document.getElementById("l" + i.toString());
+    	navpage[i] = document.getElementById("p" + i.toString());
     }
 
     var scrolled = Math.floor($(window).scrollTop() / 139);
     if(scrolled < 0) scrolled = 0; //anchor
     var circleSize = [];
+    var fontSize = [];
 
-    for(var i = 0; i < 10; i++) {
+    for(var i = 0; i < 7; i++) {
     	circleSize.push(circleResize(scrolled, i));
+    	fontSize.push(fontResize(scrolled, i));
     	$(navbar[i]).stop().animate({width: circleSize[i], 
     		height: circleSize[i], opacity: (circleSize[i] - 8)/100}, 25);
+    	$(navlinks[i]).stop().animate({width: circleSize[i], height: circleSize[i]}, 25);
+    	$(navpage[i]).stop().animate({height: circleSize[i], 
+    		 fontSize: fontSize[i] + "%", opacity: (circleSize[i] + 25)/100}, 25);
     }
 });
 
@@ -34,18 +43,23 @@ function circleResize(anchor, position) {
 	return 32*Math.pow(Math.E,(-Math.pow(position - anchor, 2))/12) + 18;
 }
 
+function fontResize(anchor, position) {
+	return 150*Math.pow(Math.E,(-Math.pow(position - anchor, 2))/12) + 95;
+}
+
 $(window).scroll(function () { 
-    var scrolled = Math.floor($(window).scrollTop() / 139);
+    var scrolled = Math.floor($(window).scrollTop() / 250);
     if(scrolled < 0) scrolled = 0; //anchor
     var circleSize = [];
+    var fontSize = [];
 
-    for(var i = 0; i < 10; i++) {
+    for(var i = 0; i < 7; i++) {
     	circleSize.push(circleResize(scrolled, i));
+    	fontSize.push(fontResize(scrolled, i));
     	$(navbar[i]).stop().animate({width: circleSize[i], 
     		height: circleSize[i], opacity: (circleSize[i] - 8)/100}, 25);
+    	$(navlinks[i]).stop().animate({width: circleSize[i], height: circleSize[i]}, 25);
+    	$(navpage[i]).stop().animate({height: circleSize[i], 
+    		 fontSize: fontSize[i] + "%", opacity: (circleSize[i] + 25)/100}, 25);
     }
-
-    /*if(navbar[0].getAttribute("width") < 75 && navbar[0].getAttribute("height") < 75) {
-    	$(navbar[0]).stop().animate({opacity: 0}, 50);
-    }*/
 });
